2f087a84a35058f398ed9f5bf78710a4a1b55e5e
Joe <joe.ekiert@gmail.com>
2024-11-26 12:25:08 -0500

    have ssr - and a little janky solution for my server-to-client mismatch

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { MantineProvider } from "@mantine/core";
 
+import { useIsHydrated } from "./hooks/useIsHydrated";
+
 import { router } from "./router";
 
 import "@mantine/core/styles.css";
@@ -9,6 +11,16 @@ import "@mantine/core/styles.css";
 const queryClient = new QueryClient();
 
 function App() {
+  /**
+   * Eventually remove a dependency on this hook
+   * I have it here because of a style element mismatch
+   * when hydrating the app on the client side
+   */
+  const hydrated = useIsHydrated();
+  if (!hydrated) {
+    return null;
+  }
+
   return (
     <MantineProvider>
       <QueryClientProvider client={queryClient}>
