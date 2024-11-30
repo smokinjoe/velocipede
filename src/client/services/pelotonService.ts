a78c4d1c90815477158162d4f521cc52a332e78f
Joe <joe.ekiert@gmail.com>
2024-11-30 18:35:47 -0500

    utilizing new internal api client

--- src/client/services/pelotonService.ts
+++ src/client/services/pelotonService.ts
@@ -1,7 +1,8 @@
-import { getPelotonApiClient } from "./pelotonApiClient";
+// import { getPelotonApiClient } from "./pelotonApiClient";
+import { getInternalApiClient } from "./internalApiClient";
 
 export const login = async (username: string, password: string) => {
-  const response = await getPelotonApiClient().post(
+  const response = await getInternalApiClient().post(
     "/auth/login",
     {
       username_or_email: username,
