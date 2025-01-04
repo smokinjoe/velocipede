69d324025f6c13aff8979080d5bc5eeb3a9e5816
Joe <joe.ekiert@gmail.com>
2025-01-03 21:47:27 -0500

    adding/updating config files to work with path aliases and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/client/**/*.{html,ts,tsx}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,23 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
+
+import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
 // import basicSsl from "@vitejs/plugin-basic-ssl";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      client: path.resolve(__dirname, "src/client"),
+      server: path.resolve(__dirname, "src/server"),
+      common: path.resolve(__dirname, "src/common"),
+    },
+  },
 });
