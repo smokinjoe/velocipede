b3d43e1944a64ec571e120d0b26395194c176799
Joe <joe.ekiert@gmail.com>
2025-01-12 17:32:23 -0500

    adding note about namespacing jotai atoms

--- src/client/components/ui/Table/usePagination.ts
+++ src/client/components/ui/Table/usePagination.ts
@@ -1,5 +1,10 @@
 import { atom, useAtom } from "jotai";
 
+/**
+ * There will eventually be a time when there's a second data table that takes
+ * advantage of pagination/limit setting. At that point I'll need to figure
+ * out how to namespace these atoms so they don't conflict.
+ */
 const pageAtom = atom(0);
 const limitAtom = atom(20);
 
