e94a0e9007a3ce42bbf2e74c4be038f0c9150800
Joe <joe.ekiert@gmail.com>
2025-01-13 22:30:31 -0500

    able to create overrides for Table cells

--- src/client/components/ui/Table/Table.tsx
+++ src/client/components/ui/Table/Table.tsx
@@ -1,12 +1,3 @@
-type TableProps = {
-  data: Record<
-    string,
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    string | number | boolean | null | Record<string, any>
-  >[];
-  columnNames?: Record<string, string>;
-};
-
 const TableHeader = ({ keys }: { keys: string[] }) => {
   return (
     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -24,6 +15,7 @@ const TableHeader = ({ keys }: { keys: string[] }) => {
 const TableRow = ({
   data,
   keys,
+  overrides,
 }: {
   data: Record<
     string,
@@ -31,28 +23,44 @@ const TableRow = ({
     string | number | boolean | null | Record<string, any>
   >;
   keys: string[];
+  overrides?: Record<string, (value: string) => JSX.Element>;
 }) => {
   const handleValue = (
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    value: string | number | boolean | null | Record<string, any>
+    value: string | number | boolean | null | Record<string, any>,
+    key: string
   ) => {
+    if (overrides && overrides[key]) {
+      return overrides[key](value as string);
+    }
+
     if (typeof value === "object" || typeof value === "boolean") {
       return JSON.stringify(value, null, 2);
     }
     return value;
   };
 
-  const body = keys.map((key: string, index: number) => {
+  const row = keys.map((key: string, index: number) => {
     return (
       <td key={`${key}-${index}`} className="px-6 py-4">
-        {handleValue(data[key])}
+        {handleValue(data[key], key)}
       </td>
     );
   });
-  return body;
+  return row;
 };
 
-export const Table = ({ data, columnNames }: TableProps) => {
+type TableProps = {
+  data: Record<
+    string,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    string | number | boolean | null | Record<string, any>
+  >[];
+  columnNames?: Record<string, string>;
+  cellOverrides?: Record<string, (value: string) => JSX.Element>;
+};
+
+export const Table = ({ data, columnNames, cellOverrides }: TableProps) => {
   // TODO: Refactor this a little more, it's pretty confusing to understand
   const tableHeaderKeys = Object.keys(data[0]).map((key) =>
     columnNames && columnNames[key] ? columnNames[key] : key
@@ -72,7 +80,11 @@ export const Table = ({ data, columnNames }: TableProps) => {
               key={index}
               className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700"
             >
-              <TableRow data={row} keys={Object.keys(data[0])} />
+              <TableRow
+                data={row}
+                keys={Object.keys(data[0])}
+                overrides={cellOverrides}
+              />
             </tr>
           ))}
         </tbody>
