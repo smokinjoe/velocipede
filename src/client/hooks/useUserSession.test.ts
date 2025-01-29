7190f00399911730b6ad1632a6dd66f36e236cfd
Joe <joe.ekiert@gmail.com>
2025-01-28 22:34:28 -0500

    useUserSession tests

---
+++ src/client/hooks/useUserSession.test.ts
@@ -0,0 +1,37 @@
+import { act, renderHook } from "@testing-library/react";
+import { useUserSession } from "./useUserSession";
+
+test("useUserSession", () => {
+  const { result } = renderHook(() => useUserSession());
+
+  expect(result.current.userSession).toEqual({
+    isLoggedIn: false,
+  });
+
+  act(() => {
+    result.current.setUserSession({
+      isLoggedIn: true,
+      userId: "userId",
+      cookies: "cookies",
+      sessionId: "sessionId",
+    });
+  });
+
+  expect(result.current.userSession).toEqual({
+    isLoggedIn: true,
+    user: {
+      id: "1",
+      userId: "userId",
+      cookies: "cookies",
+      sessionId: "sessionId",
+    },
+  });
+
+  act(() => {
+    result.current.clearSession();
+  });
+
+  expect(result.current.userSession).toEqual({
+    isLoggedIn: false,
+  });
+});
